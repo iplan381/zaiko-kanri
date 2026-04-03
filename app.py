644d299ef7d100d16dca022dfdf17c1ed7fd4ce2
iplan381 <koujou@i-plan.co.jp>
2026-04-03 17:06:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ if selected_indices:
                         summary_list.append(f"📅 **{p['type']}**: {item_detail} (予約日:{p['res_date']})")
                     else:
                         # 地名変更時も同様にスペースを空ける
-                        current_item = f"{row['商品名']}　　({row['サイズ']})　　{p['loc']}　　数量：{p['qty']}"
+                        current_item = 　f"{row['商品名']}　({row['サイズ']})　{p['loc']}　{p['qty']}"
                         loc_change = f" ※地名変更: {row['地名']} → {p['loc']}" if p["loc"] != row["地名"] else ""
                         summary_list.append(f"📝 **{p['type']}**: {current_item}{loc_change}")
             
